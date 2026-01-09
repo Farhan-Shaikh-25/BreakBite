4dea19c78c70b2ba5f48b184f4ff60baeeb3443b
Farhan-Shaikh-786 <farhanshaikh25.fs@gmail.com>
2026-01-09 22:20:47 +0530

    Order and item models created

---
+++ Backend/src/models/order.models.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        orderItems: [
+            {
+                itemId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Item',
+                    required: true,
+                },
+                itemName: {
+                    type: String,
+                    required: true
+                },
+                itemPrice: {
+                    type: Number,
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                }
+            }
+        ],
+        orderDate: {
+            type: Date,
+            required: true
+        },
+        totalAmount: {
+            type: Number,
+            default: 0
+        },
+        status: {
+            type: String,
+            enum: ["Processing", "Prepared", "Completed"],
+            required: true
+        }
+    },
+    {timestamps:true}
+)
+
+orderSchema.pre("save", function(){
+    var total = 0
+    this.orderItems.forEach(item => {
+        total += item.itemPrice * item.quantity
+    });
+    this.totalAmount = total
+})
+export const Order = new mongoose.model("Order", orderSchema)
