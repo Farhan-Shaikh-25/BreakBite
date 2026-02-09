82c578be0dcdd3a402bef2b556031003d680980f
Farhan-Shaikh-786 <farhanshaikh25.fs@gmail.com>
2026-02-09 16:53:04 +0530

    major changes 4

--- Backend/src/controllers/updateStatusController.js
+++ Backend/src/controllers/updateStatusController.js
@@ -8,16 +8,23 @@ export const updateStatusController = async (req) => {
     const order = await Order.findByIdAndUpdate({ _id : id }, {status : newStatus}, {new : true})
     if (newStatus == "Completed") {
         const user = await User.findById(order.userId);
-
+        console.log(user)
+        console.log(user.fcmToken)
         if (user && user.fcmToken) {
-            const message = {
-                notification: {
-                    title: "BreakBite: Food is Ready!",
-                    body: `Order #${order._id.toString().slice(-4)} is ready for pickup.`,
-                },
-                token: user.fcmToken, 
-            };
-            await admin.messaging().send(message);
+            try {
+                const message = {
+                    notification: {
+                        title: "BreakBite: Food is Ready!",
+                        body: `Order #${order._id.toString().slice(-4)} is ready for pickup.`,
+                    },
+                    token: user.fcmToken,
+                };
+
+                const response = await admin.messaging().send(message);
+                console.log("✅ FCM Success Response:", response); 
+            } catch (fcmError) {
+                console.error("❌ FCM Error Details:", fcmError);
+            }
             console.log("Notification sent successfully to:", user.email);
         }
     }
