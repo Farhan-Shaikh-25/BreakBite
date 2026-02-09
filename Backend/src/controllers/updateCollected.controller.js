d48a929e8ca5a2e0b88a37ccd029f0828cf99cc2
Farhan-Shaikh-786 <farhanshaikh25.fs@gmail.com>
2026-02-09 16:17:32 +0530

    major changes 2

--- Backend/src/controllers/updateCollected.controller.js
+++ Backend/src/controllers/updateCollected.controller.js
@@ -4,19 +4,6 @@ import { admin } from "../utils/firebase_app.js";
 
 export const updateCollectedController = async (req) => {
     const {id} = req.body
-    const order = await Order.findByIdAndUpdate({ _id : id }, {status : newStatus})
-    const user = await User.findById(order.userId);
-
-    if (user && user.fcmToken) {
-        const message = {
-            notification: {
-                title: "BreakBite: Food is Ready!",
-                body: `Order #${order._id.toString().slice(-4)} is ready for pickup.`,
-            },
-            token: user.fcmToken, 
-        };
-        await admin.messaging().send(message);
-        console.log("Notification sent successfully to:", user.email);
-    }
+    const order = await Order.findByIdAndUpdate({ _id : id }, {status : "Collected"})
     return "Order Collected"
 }
