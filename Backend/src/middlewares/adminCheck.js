502be7db835b156e90d4c53195624777f881b6c1
Farhan-Shaikh-786 <farhanshaikh25.fs@gmail.com>
2026-02-22 08:14:04 +0530

    Report Backend fix 2

--- Backend/src/middlewares/adminCheck.js
+++ Backend/src/middlewares/adminCheck.js
@@ -2,7 +2,7 @@ import { admin } from "../utils/firebase_app.js";
 
 export const adminCheck = async (req,res,next) => {
     const token = req.headers.authorization?.split(" ")[1]
-    if(!token) res.status(401).send("Access denied No token");
+    if(!token) res.status(401).json({"message": "Access denied No token"})
     else{
         const decodedToken = await admin.auth().verifyIdToken(token)
             if (decodedToken.email == "admin123@ad.in"){
@@ -10,7 +10,7 @@ export const adminCheck = async (req,res,next) => {
             console.log("User Auth done")
             next()
         }else{
-            res.status(401).send("Unauthorized access")
+            res.status(401).json({"message": "Unauthorized access"})
         }
     }
 }
--- Backend/src/middlewares/authCheck.js
+++ Backend/src/middlewares/authCheck.js
@@ -2,7 +2,7 @@ import { admin } from "../utils/firebase_app.js"
 
 export const authCheck = async (req,res,next) => {
     const token = req.headers.authorization?.split(" ")[1]
-    if(!token) res.status(401).send("Access denied No token");
+    if(!token) res.status(401).json({"message": "Access denied No token"});
     else{
         const decodedToken = await admin.auth().verifyIdToken(token)
         req.user = decodedToken
